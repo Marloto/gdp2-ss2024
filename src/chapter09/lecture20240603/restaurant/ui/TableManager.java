e4f080b6653774d07a88a13ff3a9c577f8015f23
Marloto <marloto@codestack.de>
2024-06-03 13:07:20 +0200

    Final State of TableManager 03.06.

--- src/chapter09/lecture20240603/restaurant/ui/TableManager.java
+++ src/chapter09/lecture20240603/restaurant/ui/TableManager.java
@@ -8,6 +8,7 @@ import chapter09.lecture20240603.restaurant.model.Reservation;
 import chapter09.lecture20240603.restaurant.model.Table;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -15,7 +16,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
@@ -27,6 +27,16 @@ public class TableManager extends Application {
     private ArrayList<Table> tables;
     private ArrayList<Article> articles;
     
+    private Table current;
+	private Label tableLabel;
+	private ListView<Reservation> reservations;
+	private ListView<String> orders;
+	private ChoiceBox<Article> selectArticle;
+	private Button addArticle;
+	private TextField date;
+	private TextField name;
+	private Button addReservation;
+    
     public void init() throws Exception {
         this.tables = new ArrayList<>();
         this.articles = new ArrayList<>();
@@ -34,6 +44,10 @@ public class TableManager extends Application {
         for(int i = 0; i < 10; i ++) {
             this.tables.add(new Table(i));
         }
+        
+        this.tables.get(0).addReservation(new Reservation("10:00", "A"));
+        this.tables.get(0).addReservation(new Reservation("20:00", "B"));
+        this.tables.get(1).addReservation(new Reservation("20:00", "C"));
         
         this.articles.add(new Article("Pizza Diavolo", 12.99));
         this.articles.add(new Article("Pizza Margherita", 9.99));
@@ -53,22 +67,25 @@ public class TableManager extends Application {
     	HBox formReservations = new HBox();
     	HBox formOrders = new HBox();
     	
-    	// Control Objekte erzeugen
-    	Label tableLabel = new Label("Tische");
-    	ListView reservations = new ListView();
-    	ListView orders = new ListView();
-    	ChoiceBox articles = new ChoiceBox();
-    	Button addArticle = new Button("Add");
-    	TextField date = new TextField();
-    	TextField name = new TextField();
-    	Button addReservation = new Button("Add");
+    	tableLabel = new Label("Tische");
+    	reservations = new ListView<>();
+    	orders = new ListView<>();
+    	selectArticle = new ChoiceBox<>();
+    	addArticle = new Button("Add");
+    	date = new TextField();
+    	name = new TextField();
+    	addReservation = new Button("Add");
     	
     	// Wichtig: Bei mehreren gleichen auch auf Schleifen zurückgreifen
     	List<Button> list = new ArrayList<>();
     	for(int i = 0; i < this.tables.size(); i ++) {
-    		Button button = new Button("Tisch " + i);
-    		list.add(button);
+    		Table table = this.tables.get(i);
+    		Button tableButton = new Button(table.toString());
+    		list.add(tableButton);
     	}
+    	
+    	// Befüllen der Artikel
+    	selectArticle.setItems(FXCollections.observableArrayList(this.articles));
     	
     	// Layout zusammenbauen
     	main.getChildren().addAll(tables, config);
@@ -77,20 +94,99 @@ public class TableManager extends Application {
     	}
     	config.getChildren().addAll(tableLabel, reservations, formReservations, orders, formOrders);
     	formReservations.getChildren().addAll(date, name, addReservation);
-    	formOrders.getChildren().addAll(articles, addArticle);
+    	formOrders.getChildren().addAll(selectArticle, addArticle);
     	
     	
     	
     	
+    	// Event-Handling
+    	//for(Button tableButton : list) {
+    	for(int i = 0; i < list.size(); i ++) {
+    		Button tableButton = list.get(i);
+    		Table table = this.tables.get(i);
+    		tableButton.setOnAction(new EventHandler<ActionEvent>() {
+				public void handle(ActionEvent event) {
+//					System.out.println(tableButton);
+//					System.out.println(event.getTarget());
+					if(current == table) {
+						current = null;
+					} else {
+						current = table;
+					}
+					refreshUi();
+				}
+
+    		});
+    	}
+    	
+    	addReservation.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				if(current != null) {					
+					String d = date.getText();
+					String n = name.getText();
+					// falls Variable und Attribut gleich benannt
+					// -> TableManager.this.date.getText()
+					current.addReservation(new Reservation(d, n));
+					// Date is valid!?
+					refreshUi();
+				}
+			}
+    	});
+    	
+    	addArticle.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				if(current != null) {
+					Article item = selectArticle.getSelectionModel().getSelectedItem();
+					current.addToBill(item);
+					refreshUi();
+				}
+			}
+    	});
     	
     	
-    	
-    	
+    	refreshUi();
     	
     	
         Scene scene = new Scene(main);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    
+    private void refreshUi() {
+    	ObservableList<Reservation> reservationItems = reservations.getItems();
+    	reservationItems.clear();
+    	
+    	ObservableList<String> orderItems = orders.getItems();
+    	orderItems.clear();
+    	
+    	if(current == null) {
+    		tableLabel.setText("No table selected");
+    		reservations.setDisable(true);
+    		orders.setDisable(true);
+    		name.setDisable(true);
+    		date.setDisable(true);
+    		selectArticle.setDisable(true);
+    		addArticle.setDisable(true);
+    		addReservation.setDisable(true);
+    	} else {
+    		reservations.setDisable(false);
+    		orders.setDisable(false);
+    		name.setDisable(false);
+    		date.setDisable(false);
+    		selectArticle.setDisable(false);
+    		addArticle.setDisable(false);
+    		addReservation.setDisable(false);
+    		
+    		tableLabel.setText(current.toString());
+    		
+    		for(Reservation r: current.getReservations()) {
+    			reservationItems.add(r);
+    		}
+    		for(Article a: current.getArticle()) {
+    			String text = current.getArticleCount(a) + "x " + a;
+    			orderItems.add(text);
+    		}
+    	}
     }
 
 }
